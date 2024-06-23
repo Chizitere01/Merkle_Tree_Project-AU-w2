538dfb4c02f72940997fd065d83c483084ee38f9
Chizitere Okey-Awuzie <chizzit45@gmail.com>
2024-06-23 04:17:56 +0100

    finished week 2 assignment

--- client/index.js
+++ client/index.js
@@ -1,17 +1,42 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
+const prompt = require("prompt-sync")();
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // Create the Merkle tree and get the root.
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot(); // console.log the root to check if necessary
+  console.log("Merkle Root:", root);
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+  // Ask user input to get the name which will be searched and be sure it is not null.
+  const name = prompt("Which name do you want to check? ").trim();
+  if (!name) {
+    console.error("You have to type a name");
+    return;
+  }
 
-  console.log({ gift });
+  // Find the index of the name and get proof from the Merkle tree.
+  const index = niceList.findIndex((n) => n === name);
+  if (index === -1) {
+    console.error("Name not found in the nice list");
+    return;
+  }
+
+  const proof = merkleTree.getProof(index);
+
+  try {
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+      name,
+      proof,
+    });
+
+    console.log({ gift });
+  } catch (error) {
+    console.error("Error fetching gift:", error);
+  }
 }
 
 main();
--- server/index.js
+++ server/index.js
@@ -1,26 +1,38 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+// Create the Merkle tree and get the root.
+const merkleTree = new MerkleTree(niceList);
+const MERKLE_ROOT = merkleTree.getRoot(); // Ensure this is in hexadecimal format
+console.log("Merkle Root:", MERKLE_ROOT);
 
-app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+app.post("/gift", (req, res) => {
+  try {
+    const { name, proof } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
+    if (!name || !proof) {
+      res.status(400).send("Invalid request: name and proof are required");
+      return;
+    }
+
+    // Verify that the proof is valid and the name is in the list
+    const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+
+    if (isInTheList) {
+      res.send("You got a toy robot!");
+    } else {
+      res.send("You are not on the list :(");
+    }
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).send("Internal server error");
   }
 });
 
